94765a86130816de98e2ff884ee62691021706d4
Max Petersen <maxpete121@gmail.com>
2023-12-01 11:08:00 -0700

    Structure

---
+++ app.js
@@ -0,0 +1,24 @@
+let playerStats = [{
+    gather: 1,
+    wallet: 0,
+}]
+
+let upgrades = [{
+    name: 'sharpen',
+    gatherAdd: 1,
+    price: 10,
+},{
+    name: 'Better Grip',
+    gatherAdd: 2,
+    price: 20,
+}];
+
+let autoUpgrades =[{
+    name: 'Lumber Apprentice',
+    gatherAddAuto: 1,
+    price: 15,
+},{
+    name: 'Chainsaw Expert',
+    gatherAddAuto: 3,
+    price: 30,
+}];
