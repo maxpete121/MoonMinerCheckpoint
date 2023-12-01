09430be359639419dea0b00f9497e217eff2dfc9
Max Petersen <maxpete121@gmail.com>
2023-12-01 14:25:49 -0700

    All Functions Done

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ let playerStats = {
     wallet: 0,
     axeSharp: 1,
     axeGrip: 1,
-    autoGather: 0,
+    apprentice: 0,
+    chainExpert: 0,
+    autoGather: 0
 }
 
 let upgrades = [{
@@ -22,7 +24,7 @@ let autoUpgrades =[{
     price: 15,
 },{
     name: 'Chainsaw Expert',
-    gatherAddAuto: 3,
+    gatherAddAuto: 4,
     price: 30,
 }];
 
@@ -41,16 +43,40 @@ function collectWood() {
 }
 
 
+setInterval(autoGather, 3000)
+function autoGather(){
+    if(playerStats.autoGather >= 1){
+        let autocash = playerStats.autoGather
+        playerStats.wallet += autocash
+        console.log(autocash)
+        autoUpdate()
+        pageUpdate()
+    }
+
+}
 
 function pageUpdate(){
 document.getElementById("cash").innerText = playerStats.wallet
-document.getElementById("gather-rate").innerText = playerStats.gather
-document.getElementById("axe-sharpness").innerText = playerStats.axeSharp
-document.getElementById("axe-grip").innerText = playerStats.axeGrip
 document.getElementById("sharp-price").innerText = upgrades[0].price
 document.getElementById("grip-price").innerText = upgrades[1].price
+document.getElementById("app-price").innerText = autoUpgrades[0].price
+document.getElementById("chain-price").innerText = autoUpgrades[1].price
 
 }
+
+function upgradeUpdate(){
+    document.getElementById("gather-rate").innerText = playerStats.gather
+    document.getElementById("app-labor").innerText = playerStats.apprentice
+    document.getElementById("axe-grip").innerText = playerStats.axeGrip
+    document.getElementById("axe-sharpness").innerText = playerStats.axeSharp
+    document.getElementById("chain-labor").innerText = playerStats.chainExpert
+}
+
+function autoUpdate(){
+    let autoNum = playerStats.autoGather
+    document.getElementById("auto-gather").innerText = autoNum
+}
+
 
 function sharpenAxe(){
     let sharpPrice = upgrades[0].price
@@ -62,6 +88,7 @@ function sharpenAxe(){
         playerStats.wallet -= sharpPrice
         upgrades[0].price += sharpPrice
         pageUpdate()
+        upgradeUpdate()
     }
 
 }
@@ -71,9 +98,38 @@ function newGrip(){
     if(playerStats.wallet >= gripPrice){
         let newGrip = upgrades[1].gatherAdd
         playerStats.gather += newGrip
-        playerStats.axeGrip += newGrip
+        playerStats.axeGrip++
         playerStats.wallet -= gripPrice
         upgrades[1].price *= 3
         pageUpdate()
+        upgradeUpdate()
+    }
+}
+
+function appBuy(){
+    let appPrice = autoUpgrades[0].price
+    if(playerStats.wallet >= appPrice){
+        let appUpgrade = autoUpgrades[0].gatherAddAuto
+        playerStats.autoGather += appUpgrade
+        playerStats.apprentice++
+        playerStats.wallet -= appPrice
+        autoUpgrades[0].price *= 2
+        autoUpdate()
+        pageUpdate()
+        upgradeUpdate()
+    }
+}
+
+function chainBuy(){
+    let chainPrice = autoUpgrades[1].price
+    if(playerStats.wallet >= chainPrice){
+        let chainUpgrade = autoUpgrades[1].gatherAddAuto
+        playerStats.autoGather += chainUpgrade
+        playerStats.chainExpert++
+        playerStats.wallet -= chainPrice
+        autoUpgrades[1].price *= 3.5
+        autoUpdate()
+        pageUpdate()
+        upgradeUpdate()
     }
 }
