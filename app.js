2b53390fa7764fd79bb9a38d2c7f5658ff519d82
Max Petersen <maxpete121@gmail.com>
2023-12-01 12:23:09 -0700

    Game fun

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let playerStats = {
     gather: 1,
     wallet: 0,
+    axeSharp: 1,
+    axeGrip: 1,
 }
 
 let upgrades = [{
@@ -24,7 +26,7 @@ let autoUpgrades =[{
 }];
 
 
-
+pageUpdate()
 
 
 function collectWood() {
@@ -39,4 +41,36 @@ function collectWood() {
 
 function pageUpdate(){
 document.getElementById("cash").innerText = playerStats.wallet
+document.getElementById("gather-rate").innerText = playerStats.gather
+document.getElementById("axe-sharpness").innerText = playerStats.axeSharp
+document.getElementById("axe-grip").innerText = playerStats.axeGrip
+document.getElementById("sharp-price").innerText = upgrades[0].price
+document.getElementById("grip-price").innerText = upgrades[1].price
+
+}
+
+function sharpenAxe(){
+    let sharpPrice = upgrades[0].price
+    if(playerStats.wallet >= sharpPrice){
+        let sharpenU = upgrades[0].gatherAdd
+        console.log(sharpenU)
+        playerStats.gather += sharpenU
+        playerStats.axeSharp += sharpenU
+        playerStats.wallet -= sharpPrice
+        upgrades[0].price += sharpPrice
+        pageUpdate()
+    }
+
+}
+
+function newGrip(){
+    let gripPrice = upgrades[1].price
+    if(playerStats.wallet >= gripPrice){
+        let newGrip = upgrades[1].gatherAdd
+        playerStats.gather += newGrip
+        playerStats.axeGrip += newGrip
+        playerStats.wallet -= gripPrice
+        upgrades[1].price *= 3
+        pageUpdate()
+    }
 }
